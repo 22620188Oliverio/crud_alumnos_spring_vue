7be9bd6f9d2afa5c541100afb8bd32d1394b90ba
Oliverio Rojas Sanchez <158488775+22620188Oliverio@users.noreply.github.com>
2025-05-09 15:35:22 -0600

    4-Creando nuestor controlador

---
+++ alumnos/src/main/java/com/oliverio/alumnos/controllers/AlumnoController.java
@@ -0,0 +1,24 @@
+package com.oliverio.alumnos.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oliverio.alumnos.model.Alumno;
+import com.oliverio.alumnos.repository.AlumnoRepository;
+
+@RestController
+@RequestMapping("/alumnos")
+public class AlumnoController {
+    @Autowired
+   private AlumnoRepository alumnoRepository;
+
+   @GetMapping("/traer-alumnos")
+   public List<Alumno> traerAlumnos() {
+       return alumnoRepository.findAll();
+   }
+
+}
