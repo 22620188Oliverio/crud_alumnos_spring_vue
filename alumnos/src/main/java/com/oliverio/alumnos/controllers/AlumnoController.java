39322cf327fade9f74ee99dcb131b3dbc6e03a65
Oliverio Rojas Sanchez <158488775+22620188Oliverio@users.noreply.github.com>
2025-05-11 16:33:09 -0600

    Act6-Creando metodo PUT en el controlador

--- alumnos/src/main/java/com/oliverio/alumnos/controllers/AlumnoController.java
+++ alumnos/src/main/java/com/oliverio/alumnos/controllers/AlumnoController.java
@@ -3,8 +3,11 @@ package com.oliverio.alumnos.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +30,23 @@ public class AlumnoController {
    public Alumno insertarAlumno(@RequestBody Alumno alumno) {
        return alumnoRepository.save(alumno);
    }
+
+   @PutMapping("/editar-alumnos/{id}")
+    public ResponseEntity<Alumno> actualizarAlumno(@PathVariable Long id, @RequestBody Alumno alumno) {
+         return alumnoRepository.findById(id)
+           .map(existingAlumno -> {
+               existingAlumno.setNumeroControl(alumno.getNumeroControl());
+               existingAlumno.setNombre(alumno.getNombre());
+               existingAlumno.setApellido(alumno.getApellido());
+               existingAlumno.setTelefono(alumno.getTelefono());
+               existingAlumno.setEmail(alumno.getEmail());
+               existingAlumno.setCarrera(alumno.getCarrera());
+               existingAlumno.setImagenURL(alumno.getImagenURL());
+               Alumno updatedAlumno = alumnoRepository.save(existingAlumno);
+               return ResponseEntity.ok(updatedAlumno);
+           })
+           .orElse(ResponseEntity.notFound().build());
+    }
+    
 
 }
