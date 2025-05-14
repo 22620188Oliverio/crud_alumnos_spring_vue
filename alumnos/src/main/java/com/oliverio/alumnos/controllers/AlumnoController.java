142f987cdcac2e668b106f657b4c94704b505311
Oliverio Rojas Sanchez <158488775+22620188Oliverio@users.noreply.github.com>
2025-05-14 02:56:10 -0600

    Act12-Creando metodo GET para traer la informacion de los alumnos

--- alumnos/src/main/java/com/oliverio/alumnos/controllers/AlumnoController.java
+++ alumnos/src/main/java/com/oliverio/alumnos/controllers/AlumnoController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.oliverio.alumnos.repository.AlumnoRepository;
 
 @RestController
 @RequestMapping("/alumnos")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class AlumnoController {
     @Autowired
    private AlumnoRepository alumnoRepository;
--- front_alumnos/src/App.vue
+++ front_alumnos/src/App.vue
@@ -1,13 +1,28 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
+const alumnos = ref([]);
+
+const cargarAlumnos = async () => {
+  try {
+    const response = await axios.get('http://localhost:8080/alumnos/traer-alumnos');
+    alumnos.value = response.data;
+    console.log(alumnos.value);
+  } catch (error) {
+    console.error("Error al cargar alumnos:", error);
+  }
+};
+
+onMounted(cargarAlumnos);
 </script>
 
 <template>
-<div class="container">
-  <button class="btn btn-primary" >
-    boton
-  </button>
-</div>
+  <div class="container">
+    <button class="btn btn-primary">
+      boton
+    </button>
+  </div>
 </template>
 
 <style scoped></style>
